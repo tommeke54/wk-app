a0baa0e74a28fab1cdbbe9e937d4fb9b195d32be
tommeke54 <tomvanhees89@gmail.com>
2026-06-11 15:21:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -76,16 +76,39 @@ function showMatches() {
 
 app.innerHTML = `
 
-<div class="card">
-<h2>⚽ Wedstrijden</h2>
+<div class="card match-card">
 
-🇧🇪 België vs Frankrijk 🇫🇷
+<h2>⚽ Wedstrijd 1</h2>
+
+🇧🇪 België
+
+<div class="vs">VS</div>
+
+🇫🇷 Frankrijk
+
 <br><br>
 
-🇧🇷 Brazilië vs Japan 🇯🇵
+📅 20 juni 2026<br>
+🕘 21:00<br>
+🏟 MetLife Stadium
+
+</div>
+
+<div class="card match-card">
+
+<h2>⚽ Wedstrijd 2</h2>
+
+🇧🇷 Brazilië
+
+<div class="vs">VS</div>
+
+🇯🇵 Japan
+
 <br><br>
 
-🇩🇪 Duitsland vs Spanje 🇪🇸
+📅 21 juni 2026<br>
+🕘 18:00<br>
+🏟 Estadio Azteca
 
 </div>
 
