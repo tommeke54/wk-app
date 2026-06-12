aa173a026d0da38308e4ffcfa8ca79da6eaebfa3
tommeke54 <tomvanhees89@gmail.com>
2026-06-12 18:24:34 +0200

    Update data-matches.js

---
+++ data-matches.js
@@ -0,0 +1,32 @@
+const matchesData = [
+  {
+    group: "G",
+    status: "GEPLAND",
+    date: "21 juni 2026",
+    timeBE: "20:00",
+    home: "België 🇧🇪",
+    away: "Iran 🇮🇷",
+    score: "VS",
+    stadium: "New York New Jersey Stadium"
+  },
+  {
+    group: "G",
+    status: "GEPLAND",
+    date: "16 juni 2026",
+    timeBE: "20:00",
+    home: "België 🇧🇪",
+    away: "Egypte 🇪🇬",
+    score: "VS",
+    stadium: "Seattle Stadium"
+  },
+  {
+    group: "G",
+    status: "GEPLAND",
+    date: "26 juni 2026",
+    timeBE: "20:00",
+    home: "Nieuw-Zeeland 🇳🇿",
+    away: "België 🇧🇪",
+    score: "VS",
+    stadium: "Philadelphia Stadium"
+  }
+];
