150bfd68fec675d5b94448a82f2b36f8ab215424
tommeke54 <tomvanhees89@gmail.com>
2026-06-12 22:05:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -307,8 +307,17 @@ function showStadiums() {
 function showKnockout() {
   app.innerHTML = `
     <div class="card">
-      <h2>🏆 TEST</h2>
-      Knock-out knop werkt!
+      <h2>🏆 Knock-out Center Pro</h2>
+      Knock-outfase geladen uit data-knockout.js
+    </div>
+
+    <div class="card">
+      <h2>🏆 Finale</h2>
+      Finalist 1
+      <div class="vs">VS</div>
+      Finalist 2<br><br>
+      📅 19 juli 2026<br>
+      🏟 New York New Jersey Stadium
     </div>
   `;
 }
