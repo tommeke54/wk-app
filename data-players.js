f78865793bebd78b0d47e3f3df74770d37199d08
tommeke54 <tomvanhees89@gmail.com>
2026-06-12 03:54:38 +0200

    Update data-players.js

---
+++ data-players.js
@@ -0,0 +1,80 @@
+const playersData = [
+
+  // BELGIË
+  {
+    country: "België",
+    players: [
+      { name: "Kevin De Bruyne", position: "Middenvelder", club: "Manchester City" },
+      { name: "Romelu Lukaku", position: "Aanvaller", club: "Napoli" },
+      { name: "Jérémy Doku", position: "Vleugelspeler", club: "Manchester City" },
+      { name: "Thibaut Courtois", position: "Doelman", club: "Real Madrid" }
+    ]
+  },
+
+  // FRANKRIJK
+  {
+    country: "Frankrijk",
+    players: [
+      { name: "Kylian Mbappé", position: "Aanvaller", club: "Real Madrid" },
+      { name: "Ousmane Dembélé", position: "Vleugelspeler", club: "PSG" },
+      { name: "Eduardo Camavinga", position: "Middenvelder", club: "Real Madrid" },
+      { name: "Antoine Griezmann", position: "Aanvaller", club: "Atlético Madrid" }
+    ]
+  },
+
+  // BRAZILIË
+  {
+    country: "Brazilië",
+    players: [
+      { name: "Vinícius Júnior", position: "Vleugelspeler", club: "Real Madrid" },
+      { name: "Rodrygo", position: "Aanvaller", club: "Real Madrid" },
+      { name: "Bruno Guimarães", position: "Middenvelder", club: "Newcastle United" },
+      { name: "Marquinhos", position: "Verdediger", club: "PSG" }
+    ]
+  },
+
+  // DUITSLAND
+  {
+    country: "Duitsland",
+    players: [
+      { name: "Jamal Musiala", position: "Middenvelder", club: "Bayern München" },
+      { name: "Florian Wirtz", position: "Middenvelder", club: "Bayer Leverkusen" },
+      { name: "Joshua Kimmich", position: "Middenvelder", club: "Bayern München" },
+      { name: "Kai Havertz", position: "Aanvaller", club: "Arsenal" }
+    ]
+  },
+
+  // NEDERLAND
+  {
+    country: "Nederland",
+    players: [
+      { name: "Virgil van Dijk", position: "Verdediger", club: "Liverpool" },
+      { name: "Frenkie de Jong", position: "Middenvelder", club: "FC Barcelona" },
+      { name: "Cody Gakpo", position: "Aanvaller", club: "Liverpool" },
+      { name: "Xavi Simons", position: "Middenvelder", club: "RB Leipzig" }
+    ]
+  },
+
+  // SPANJE
+  {
+    country: "Spanje",
+    players: [
+      { name: "Rodri", position: "Middenvelder", club: "Manchester City" },
+      { name: "Pedri", position: "Middenvelder", club: "FC Barcelona" },
+      { name: "Lamine Yamal", position: "Vleugelspeler", club: "FC Barcelona" },
+      { name: "Nico Williams", position: "Vleugelspeler", club: "Athletic Club" }
+    ]
+  },
+
+  // ARGENTINIË
+  {
+    country: "Argentinië",
+    players: [
+      { name: "Lionel Messi", position: "Aanvaller", club: "Inter Miami" },
+      { name: "Lautaro Martínez", position: "Aanvaller", club: "Inter Milan" },
+      { name: "Julián Álvarez", position: "Aanvaller", club: "Atlético Madrid" },
+      { name: "Emiliano Martínez", position: "Doelman", club: "Aston Villa" }
+    ]
+  }
+
+];
