19946e415acb6ffcb48517f83eec1cec521716b3
tommeke54 <tomvanhees89@gmail.com>
2026-06-12 18:49:38 +0200

    Update data-standings.js

---
+++ data-standings.js
@@ -0,0 +1,20 @@
+const standingsData = [
+  {
+    group: "G",
+    teams: [
+      { team: "België 🇧🇪", played: 0, points: 0 },
+      { team: "Egypte 🇪🇬", played: 0, points: 0 },
+      { team: "Iran 🇮🇷", played: 0, points: 0 },
+      { team: "Nieuw-Zeeland 🇳🇿", played: 0, points: 0 }
+    ]
+  },
+  {
+    group: "L",
+    teams: [
+      { team: "Engeland 🏴", played: 0, points: 0 },
+      { team: "Kroatië 🇭🇷", played: 0, points: 0 },
+      { team: "Ghana 🇬🇭", played: 0, points: 0 },
+      { team: "Panama 🇵🇦", played: 0, points: 0 }
+    ]
+  }
+];
