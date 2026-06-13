83265aaf53cdc869383cec9ab75b25d1896a4578
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 13:23:00 +0200

    Update data-standings.js

--- data-standings.js
+++ data-standings.js
@@ -27,7 +27,68 @@ const standingsData = [
       { team: "Haïti 🇭🇹", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
       { team: "Schotland 🏴", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
     ]
+  ,
+
+  {
+    group: "G",
+    teams: [
+      { team: "België 🇧🇪", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Egypte 🇪🇬", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Iran 🇮🇷", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Nieuw-Zeeland 🇳🇿", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
   },
+
+  {
+    group: "H",
+    teams: [
+      { team: "Spanje 🇪🇸", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Kaapverdië 🇨🇻", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Saudi-Arabië 🇸🇦", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Uruguay 🇺🇾", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
+  },
+
+  {
+    group: "I",
+    teams: [
+      { team: "Frankrijk 🇫🇷", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Senegal 🇸🇳", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Irak 🇮🇶", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Noorwegen 🇳🇴", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
+  },
+
+  {
+    group: "J",
+    teams: [
+      { team: "Argentinië 🇦🇷", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Algerije 🇩🇿", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Oostenrijk 🇦🇹", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Jordanië 🇯🇴", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
+  },
+
+  {
+    group: "K",
+    teams: [
+      { team: "Portugal 🇵🇹", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "DR Congo 🇨🇩", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Oezbekistan 🇺🇿", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Colombia 🇨🇴", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
+  },
+
+  {
+    group: "L",
+    teams: [
+      { team: "Engeland 🏴", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Kroatië 🇭🇷", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Ghana 🇬🇭", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+      { team: "Panama 🇵🇦", played: 0, wins: 0, draws: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 }
+    ]
+  }
+];
 
   {
     group: "D",
