36f29d63b6530fe88c9068a5ee8bb3364dd6a252
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 04:03:01 +0200

    Update data-matchdays.js

---
+++ data-matchdays.js
@@ -0,0 +1,14 @@
+const matchDaysData = [
+  {
+    day: "Speeldag 1",
+    matches: []
+  },
+  {
+    day: "Speeldag 2",
+    matches: []
+  },
+  {
+    day: "Speeldag 3",
+    matches: []
+  }
+];
