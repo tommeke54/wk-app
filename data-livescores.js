6523b51aa9fb4be6b1ef9559822af049504dbb30
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 02:43:59 +0200

    Update data-livescores.js

---
+++ data-livescores.js
@@ -0,0 +1,18 @@
+const liveScoresData = [
+  {
+    status: "LIVE",
+    home: "Nederland 🇳🇱",
+    away: "Argentinië 🇦🇷",
+    score: "1 - 1",
+    minute: "67",
+    lastGoal: "Argentinië"
+  },
+  {
+    status: "AFGELOPEN",
+    home: "Mexico 🇲🇽",
+    away: "Zuid-Afrika 🇿🇦",
+    score: "2 - 0",
+    minute: "90",
+    lastGoal: "Mexico"
+  }
+];
