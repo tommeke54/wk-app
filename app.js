b61a58a737a65a3705111ebde1c7181eca470bb2
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 12:44:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -464,7 +464,6 @@ function showMore() {
     <div class="card"><button onclick="showTeams()">👥 Teams</button></div>
     <div class="card"><button onclick="showStadiums()">🏟 Stadions</button></div>
     <div class="card"><button onclick="showStandings()">📊 Standen</button></div>
-    <div class="card"><button onclick="showKnockout()">🏆 Knock-out</button></div>
     <div class="card"><button onclick="showNews()">📰 Nieuws</button></div>
     <div class="card"><button onclick="showNotifications()">🔔 Meldingen</button></div>
   `;
