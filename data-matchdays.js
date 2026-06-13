6fb2d28cfbedea4b87982447e031cb4521891142
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 04:35:40 +0200

    Update data-matchdays.js

--- data-matchdays.js
+++ data-matchdays.js
@@ -1,12 +1,45 @@
 const matchDaysData = [
   {
     day: "Speeldag 1",
-    matches: []
+    matches: [
+      { group:"A", status:"AFGELOPEN", date:"11 juni 2026", timeBE:"21:00", home:"Mexico 🇲🇽", away:"Zuid-Afrika 🇿🇦", score:"2 - 0", stadium:"Mexico City Stadium" },
+      { group:"A", status:"AFGELOPEN", date:"12 juni 2026", timeBE:"04:00", home:"Zuid-Korea 🇰🇷", away:"Tsjechië 🇨🇿", score:"2 - 1", stadium:"Guadalajara Stadium" },
+
+      { group:"B", status:"AFGELOPEN", date:"12 juni 2026", timeBE:"21:00", home:"Canada 🇨🇦", away:"Bosnië en Herzegovina 🇧🇦", score:"1 - 1", stadium:"Toronto Stadium" },
+      { group:"D", status:"LIVE", date:"13 juni 2026", timeBE:"03:00", home:"Verenigde Staten 🇺🇸", away:"Paraguay 🇵🇾", score:"1 - 0", stadium:"Los Angeles Stadium" },
+
+      { group:"B", status:"GEPLAND", date:"13 juni 2026", timeBE:"21:00", home:"Qatar 🇶🇦", away:"Zwitserland 🇨🇭", score:"VS", stadium:"Nog aan te vullen" },
+      { group:"C", status:"GEPLAND", date:"14 juni 2026", timeBE:"00:00", home:"Brazilië 🇧🇷", away:"Marokko 🇲🇦", score:"VS", stadium:"Nog aan te vullen" },
+      { group:"C", status:"GEPLAND", date:"14 juni 2026", timeBE:"03:00", home:"Haïti 🇭🇹", away:"Schotland 🏴", score:"VS", stadium:"Boston Stadium" },
+      { group:"D", status:"GEPLAND", date:"14 juni 2026", timeBE:"06:00", home:"Australië 🇦🇺", away:"Turkije 🇹🇷", score:"VS", stadium:"Nog aan te vullen" },
+
+      { group:"E", status:"GEPLAND", date:"14 juni 2026", timeBE:"19:00", home:"Duitsland 🇩🇪", away:"Curaçao 🇨🇼", score:"VS", stadium:"Nog aan te vullen" },
+      { group:"F", status:"GEPLAND", date:"14 juni 2026", timeBE:"22:00", home:"Nederland 🇳🇱", away:"Japan 🇯🇵", score:"VS", stadium:"Nog aan te vullen" },
+      { group:"E", status:"GEPLAND", date:"15 juni 2026", timeBE:"01:00", home:"Ivoorkust 🇨🇮", away:"Ecuador 🇪🇨", score:"VS", stadium:"Nog aan te vullen" },
+      { group:"F", status:"GEPLAND", date:"15 juni 2026", timeBE:"04:00", home:"Zweden 🇸🇪", away:"Tunesië 🇹🇳", score:"VS", stadium:"Monterrey Stadium" },
+
+      { group:"H", status:"GEPLAND", date:"15 juni 2026", timeBE:"18:00", home:"Spanje 🇪🇸", away:"Kaapverdië 🇨🇻", score:"VS", stadium:"Atlanta Stadium" },
+      { group:"G", status:"GEPLAND", date:"15 juni 2026", timeBE:"21:00", home:"België 🇧🇪", away:"Egypte 🇪🇬", score:"VS", stadium:"Seattle Stadium" },
+      { group:"H", status:"GEPLAND", date:"16 juni 2026", timeBE:"00:00", home:"Saudi-Arabië 🇸🇦", away:"Uruguay 🇺🇾", score:"VS", stadium:"Miami Stadium" },
+      { group:"G", status:"GEPLAND", date:"16 juni 2026", timeBE:"03:00", home:"Iran 🇮🇷", away:"Nieuw-Zeeland 🇳🇿", score:"VS", stadium:"Los Angeles Stadium" },
+
+      { group:"I", status:"GEPLAND", date:"16 juni 2026", timeBE:"21:00", home:"Frankrijk 🇫🇷", away:"Senegal 🇸🇳", score:"VS", stadium:"New York New Jersey Stadium" },
+      { group:"I", status:"GEPLAND", date:"17 juni 2026", timeBE:"00:00", home:"Irak 🇮🇶", away:"Noorwegen 🇳🇴", score:"VS", stadium:"Boston Stadium" },
+      { group:"J", status:"GEPLAND", date:"17 juni 2026", timeBE:"03:00", home:"Argentinië 🇦🇷", away:"Algerije 🇩🇿", score:"VS", stadium:"Kansas City Stadium" },
+      { group:"J", status:"GEPLAND", date:"17 juni 2026", timeBE:"06:00", home:"Oostenrijk 🇦🇹", away:"Jordanië 🇯🇴", score:"VS", stadium:"San Francisco Bay Area Stadium" },
+
+      { group:"K", status:"GEPLAND", date:"17 juni 2026", timeBE:"19:00", home:"Portugal 🇵🇹", away:"DR Congo 🇨🇩", score:"VS", stadium:"Houston Stadium" },
+      { group:"L", status:"GEPLAND", date:"17 juni 2026", timeBE:"22:00", home:"Engeland 🏴", away:"Kroatië 🇭🇷", score:"VS", stadium:"Dallas Stadium" },
+      { group:"L", status:"GEPLAND", date:"18 juni 2026", timeBE:"01:00", home:"Ghana 🇬🇭", away:"Panama 🇵🇦", score:"VS", stadium:"Toronto Stadium" },
+      { group:"K", status:"GEPLAND", date:"18 juni 2026", timeBE:"04:00", home:"Oezbekistan 🇺🇿", away:"Colombia 🇨🇴", score:"VS", stadium:"Mexico City Stadium" }
+    ]
   },
+
   {
     day: "Speeldag 2",
     matches: []
   },
+
   {
     day: "Speeldag 3",
     matches: []
