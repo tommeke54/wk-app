c2b45342195cc1bd9da2958cb69fa7ec0da8aa65
tommeke54 <tomvanhees89@gmail.com>
2026-06-13 02:01:59 +0200

    Update data-knockout.js

---
+++ data-knockout.js
@@ -0,0 +1,32 @@
+const knockoutData = [
+  {
+    round: "1/16 finales",
+    matches: []
+  },
+  {
+    round: "Achtste finales",
+    matches: []
+  },
+  {
+    round: "Kwartfinales",
+    matches: []
+  },
+  {
+    round: "Halve finales",
+    matches: []
+  },
+  {
+    round: "Finale",
+    matches: [
+      {
+        status: "Gepland",
+        date: "19 juli 2026",
+        timeBE: "Nog te bepalen",
+        home: "Finalist 1",
+        away: "Finalist 2",
+        score: "VS",
+        stadium: "New York New Jersey Stadium"
+      }
+    ]
+  }
+];
